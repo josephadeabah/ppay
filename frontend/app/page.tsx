d5106e71bb46b1b9daf1e3d017c0029cf6cfb5c5
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-07-15 20:19:55 +0000

    feat: add about us

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,112 +1,63 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen items-center justify-center bg-blue-100">
+      <div className="flex flex-col w-full h-screen">
+        <div className="flex flex-1 flex-col lg:flex-row">
+          <div className="flex flex-col space-y-0 w-full lg:w-1/3">
+            <div className="flex-1 bg-blue-950 p-4">
+              <h2 className="text-lg font-bold text-white">Why This System?</h2>
+              <p className="text-white">
+                Professionals in developing world are often compensated less than their counterparts in the developed world, despite most times doing the same job and in the same company or otherwise.
+              </p>
+            </div>
+            <div className="flex-1 bg-blue-700 p-4">
+              <h2 className="text-lg font-bold text-white">The Issue</h2>
+              <p className="text-white">
+                This pay disparity, known as &rsquo;geographic pay discrimination&rsquo; or &rsquo;pay arbitrage&rsquo;, is unfair and driven by market forces exploiting lower living costs and weaker labor protections in developing countries.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-0 w-full lg:w-1/3">
+            <div className="flex-[2] bg-blue-950 p-4">
+              <h2 className="text-lg font-bold text-white text-4xl">Global Wage Transparency</h2>
+              <p className="text-white">
+                A platform where Professionals can anonymously report their wages and job descriptions to compare pay across different countries.
+              </p>
+            </div>
+            <div className="flex-[1] bg-blue-500 p-4">
+              <h2 className="text-lg font-bold text-white">Impact</h2>
+              <p className="text-white">
+              Fair Wage Negotiation: The tool empowers employees to negotiate fair salaries aligned with recommended pay standards.
+              </p>
+              <p className="text-white">
+              Standardized Pay Practices: Promotes adoption of uniform pay practices across companies, mitigating disparities in pay based on geographical location
+              </p>
+              <p className="text-white">
+              Enhanced Employer Branding: Companies implementing fair pay practices enhance their appeal to top talent, highlighting their dedication to equitable wages.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-0 w-full lg:w-1/3">
+            <div className="flex-1 bg-blue-600 p-4">
+              <h2 className="text-lg font-bold text-white">Pay Equity Calculator</h2>
+              <p className="text-white">
+                A tool that compares wages across countries, providing fair wage recommendations based on qualifications and job responsibilities, excluding cost of living or geographical location.
+              </p>
+            </div>
+            <div className="flex-1 bg-blue-800 p-4">
+              <h2 className="text-lg font-bold text-white">Fair Wage Negotiation</h2>
+              <p className="text-white">
+                Employees can use this tool to negotiate better salaries, encouraging standardized pay practices and enhancing employer branding.
+              </p>
+            </div>
+            <div className="flex-1 bg-blue-800 p-4">
+              <h2 className="text-lg font-bold text-white">Join Us</h2>
+              <p className="text-white">
+                Together, we can address geographic pay discrimination and ensure fair compensation for all professionals.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
