9eb66483126462bff8df0a2c7ab3e3b55299281b
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-07-19 22:13:19 +0000

    fix: make navbar globally accessible

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -11,27 +11,21 @@ const DashboardLayout: FC<PropsWithChildren> = function ({ children }) {
   const { isCollapsed } = useSidebarContext();
 
   return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <DashboardSidebar />
-
-      <div
-        className={twMerge(
-          "flex-1 flex flex-col transition-all duration-300",
-          isCollapsed ? "lg:ml-16" : "lg:ml-64", // Adjust according to sidebar width
-        )}
-      >
-        {/* Navbar */}
-        <DashboardNavbar />
-        
-        <main
+    <>
+      <DashboardNavbar />
+      <div className="mt-16 flex items-start">
+        <DashboardSidebar />
+        <div
           id="main-content"
-          className="relative flex-1 overflow-y-auto bg-gray-50 dark:bg-gray-900"
+          className={twMerge(
+            "relative h-full w-full overflow-y-auto bg-gray-50 dark:bg-gray-900",
+            isCollapsed ? "lg:ml-[4.5rem]" : "lg:ml-64",
+          )}
         >
           {children}
-        </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
