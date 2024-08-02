00c3182edd73a138582c25fac743e06ec2e395d7
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-08-02 11:02:38 +0000

    fix: get single user by id

--- backend/app/controllers/api/v1/members/users_controller.rb
+++ backend/app/controllers/api/v1/members/users_controller.rb
@@ -1,22 +1,26 @@
 module Api
-    module V1
-      module Members
-        class UsersController < ApplicationController
-          before_action :authenticate_request, except: [:index]  # Skip authentication for index action
-          before_action :authorize_admin, only: [:make_admin]
-          before_action :set_user, only: [:change_password, :make_admin]
+  module V1
+    module Members
+      class UsersController < ApplicationController
+        before_action :authenticate_request, except: [:index]  # Skip authentication for index action
+        before_action :authorize_admin, only: [:make_admin]
+        before_action :set_user, only: [:change_password, :make_admin, :show_by_id]
 
-          # GET /api/v1/members/users
+        # GET /api/v1/members/users
         def index
           @users = User.includes(:profile).all
           render json: @users.to_json(include: :profile), status: :ok
         end
 
-          # GET /api/v1/members/users/me
-          # GET /api/v1/members/users/:id
-          def show
-            render json: @current_user.as_json(include: :profile), status: :ok
-          end
+        # GET /api/v1/members/users/me
+        def show
+          render json: @current_user.as_json(include: :profile), status: :ok
+        end
+
+        # GET /api/v1/members/users/:id
+        def show_by_id
+          render json: @user.as_json(include: :profile), status: :ok
+        end
 
         # PUT /api/v1/members/users/:id/make_admin
         def make_admin
@@ -28,41 +32,40 @@ module Api
           end
         end
 
-          # PUT /api/v1/members/users/me
-          def update
-            if @current_user.update(user_params)
-              render json: @current_user.as_json(include: :profile), status: :ok
-            else
-              render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
-            end
+        # PUT /api/v1/members/users/me
+        def update
+          if @current_user.update(user_params)
+            render json: @current_user.as_json(include: :profile), status: :ok
+          else
+            render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
           end
+        end
 
-
-          # PUT /api/v1/members/users/me/password
-          def change_password
-            if @user.authenticate(params[:current_password]) && @user.update(password_params)
-              render json: { message: 'Password updated successfully' }, status: :ok
-            else
-              render json: { error: 'Current password is incorrect or new password is invalid' }, status: :unprocessable_entity
-            end
+        # PUT /api/v1/members/users/me/password
+        def change_password
+          if @user.authenticate(params[:current_password]) && @user.update(password_params)
+            render json: { message: 'Password updated successfully' }, status: :ok
+          else
+            render json: { error: 'Current password is incorrect or new password is invalid' }, status: :unprocessable_entity
           end
+        end
 
-          private
+        private
 
-          def set_user
-            @user = User.includes(:profile).find(params[:id])
-          rescue ActiveRecord::RecordNotFound
-            render json: { error: 'User not found' }, status: :not_found
-          end
+        def set_user
+          @user = User.includes(:profile).find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          render json: { error: 'User not found' }, status: :not_found
+        end
 
-          def user_params
-            params.require(:user).permit(:email, :password, :password_confirmation, :admin, profile_attributes: [:name, :role, :status, :salary_role, :experience, :country, :industry, :category, :company, :actual_salary, :job_website, :avatar])
-          end
+        def user_params
+          params.require(:user).permit(:email, :password, :password_confirmation, :admin, profile_attributes: [:name, :role, :status, :salary_role, :experience, :country, :industry, :category, :company, :actual_salary, :job_website, :avatar])
+        end
 
-          def password_params
-            params.require(:user).permit(:password, :password_confirmation)
-          end
+        def password_params
+          params.require(:user).permit(:password, :password_confirmation)
         end
       end
     end
   end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
         post 'auth/login', to: 'auth#login'
         post 'auth/password/reset', to: 'auth#password_reset'
         put 'auth/password/reset', to: 'auth#reset_password'
-        get 'users', to: 'users#index'        # Route to get all users
+        get 'users', to: 'users#index'         # Route to get all users
         get 'users/me', to: 'users#show'       # Route for the current authenticated user
         put 'users/me', to: 'users#update'
         put 'users/me/password', to: 'users#change_password'
-        get 'users/:id', to: 'users#show'      # Route to get user by ID
+        get 'users/:id', to: 'users#show_by_id' # Route to get user by ID
         put 'users/:id/make_admin', to: 'users#make_admin'  # Route to make user an admin
       end
       namespace :locations do
