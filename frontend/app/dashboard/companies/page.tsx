824994e0891b485a6e8b17a65408d495b4380ad4
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-08-02 22:29:51 +0000

    chore: redesign companies page

--- frontend/app/dashboard/companies/page.tsx
+++ frontend/app/dashboard/companies/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import { Accordion, AccordionItem, Card } from "@nextui-org/react";
+import React from "react";
+import { HiOutlineArrowDown, HiOutlineArrowUp } from "react-icons/hi";
 
 interface Company {
   name: string;
@@ -15,7 +17,7 @@ interface Company {
 
 const companies: Company[] = [
   {
-    name: "Company A",
+    name: "Google",
     logo: "https://img.logo.dev/google.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 120,
     salaries: 50,
@@ -26,7 +28,7 @@ const companies: Company[] = [
     description: "A leading tech company.",
   },
   {
-    name: "Company B",
+    name: "Facebook",
     logo: "https://img.logo.dev/facebook.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 98,
     salaries: 40,
@@ -37,7 +39,7 @@ const companies: Company[] = [
     description: "A top financial firm.",
   },
   {
-    name: "Company C",
+    name: "Yandex",
     logo: "https://img.logo.dev/yandex.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 150,
     salaries: 60,
@@ -48,7 +50,7 @@ const companies: Company[] = [
     description: "A renowned healthcare company.",
   },
   {
-    name: "Company D",
+    name: "Apple",
     logo: "https://img.logo.dev/apple.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 110,
     salaries: 55,
@@ -59,7 +61,7 @@ const companies: Company[] = [
     description: "A pioneering automotive company.",
   },
   {
-    name: "Company E",
+    name: "Andela",
     logo: "https://img.logo.dev/andela.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 120,
     salaries: 50,
@@ -70,7 +72,7 @@ const companies: Company[] = [
     description: "A leading tech company.",
   },
   {
-    name: "Company F",
+    name: "Airbnb",
     logo: "https://img.logo.dev/airbnb.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 100,
     salaries: 45,
@@ -81,7 +83,7 @@ const companies: Company[] = [
     description: "A top financial firm.",
   },
   {
-    name: "Company G",
+    name: "Zoom",
     logo: "https://img.logo.dev/zoom.us?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 80,
     salaries: 35,
@@ -92,7 +94,7 @@ const companies: Company[] = [
     description: "A leading educational institution.",
   },
   {
-    name: "Company H",
+    name: "Verizon",
     logo: "https://img.logo.dev/verizon.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 120,
     salaries: 50,
@@ -103,7 +105,7 @@ const companies: Company[] = [
     description: "A leading tech company.",
   },
   {
-    name: "Company I",
+    name: "Twitter",
     logo: "https://img.logo.dev/twitter.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 80,
     salaries: 35,
@@ -114,7 +116,7 @@ const companies: Company[] = [
     description: "A leading educational institution.",
   },
   {
-    name: "Company J",
+    name: "Instagram",
     logo: "https://img.logo.dev/instagram.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 120,
     salaries: 50,
@@ -125,7 +127,7 @@ const companies: Company[] = [
     description: "A leading tech company.",
   },
   {
-    name: "Company K",
+    name: "Telegram",
     logo: "https://img.logo.dev/telegram.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 80,
     salaries: 35,
@@ -136,7 +138,7 @@ const companies: Company[] = [
     description: "A leading educational institution.",
   },
   {
-    name: "Company L",
+    name: "mPharma",
     logo: "https://img.logo.dev/mpharma.com?token=pk_EoYVBqzrSFeSm5-xN9WT9Q",
     reviews: 120,
     salaries: 50,
@@ -149,145 +151,63 @@ const companies: Company[] = [
   // Add more company objects as needed
 ];
 
-const CompanyCard: React.FC<{ company: Company; onClick: () => void }> = ({
-  company,
-  onClick,
-}) => {
-  return (
-    <div
-      className="cursor-pointer rounded-sm bg-white p-4 shadow-sm dark:bg-gray-800"
-      onClick={onClick}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") onClick();
-      }}
-      tabIndex={0}
-      role="button"
-      aria-label={`View details for ${company.name}`}
-      title={`View details for ${company.name}`}
-      style={{ transition: "background-color 0.3s ease" }}
-      data-testid={`company-${company.name}`}
-    >
-      <div className="mb-4 flex items-center">
-        <img
-          src={company.logo}
-          alt={`${company.name} logo`}
-          width={50}
-          height={50}
-          className="rounded-full"
-        />
-        <h2 className="ml-4 text-xl font-semibold text-gray-900 dark:text-gray-100">
-          {company.name}
-        </h2>
-      </div>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Location:</strong> {company.location}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Global Company Size:</strong> {company.size}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Industry:</strong> {company.industry}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Reviews:</strong> {company.reviews}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Salaries:</strong> {company.salaries}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Jobs:</strong> {company.jobs}
-      </p>
-      <p className="mt-2 text-gray-700 dark:text-gray-300">
-        {company.description}
-      </p>
-    </div>
-  );
-};
-
-const CompanyDetails: React.FC<{ company: Company; onBack: () => void }> = ({
-  company,
-  onBack,
-}) => {
-  return (
-    <div className="rounded-sm bg-white p-4 shadow-sm dark:bg-gray-800">
-      <button
-        onClick={onBack}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") onBack();
-        }}
-        tabIndex={0}
-        className="mb-4 text-blue-500"
-        aria-label="Back to company list"
-        title="Back to company list"
-      >
-        Back
-      </button>
-      <div className="mb-4 flex items-center">
-        <img
-          src={company.logo}
-          alt={`${company.name} logo`}
-          width={50}
-          height={50}
-          className="rounded-full"
-        />
-        <h2 className="ml-4 text-xl font-semibold text-gray-900 dark:text-gray-100">
-          {company.name}
-        </h2>
-      </div>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Location:</strong> {company.location}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Global Company Size:</strong> {company.size}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Industry:</strong> {company.industry}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Reviews:</strong> {company.reviews}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Salaries:</strong> {company.salaries}
-      </p>
-      <p className="mb-1 text-gray-700 dark:text-gray-300">
-        <strong>Jobs:</strong> {company.jobs}
-      </p>
-      <p className="mt-2 text-gray-700 dark:text-gray-300">
-        {company.description}
-      </p>
-    </div>
-  );
-};
-
 const CompaniesPage: React.FC = () => {
-  const [selectedCompany, setSelectedCompany] = useState<Company | null>(null);
-
-  const handleCardClick = (company: Company) => {
-    setSelectedCompany(company);
-  };
-
-  const handleBackClick = () => {
-    setSelectedCompany(null);
-  };
-
   return (
     <div className="mx-auto bg-neutral-50 p-2 dark:bg-gray-900">
       <h1 className="mb-4 text-2xl font-bold text-gray-900 dark:text-gray-100">
         Companies
       </h1>
-      {selectedCompany ? (
-        <CompanyDetails company={selectedCompany} onBack={handleBackClick} />
-      ) : (
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {companies.map((company, index) => (
-            <CompanyCard
-              key={index}
-              company={company}
-              onClick={() => handleCardClick(company)}
-            />
-          ))}
-        </div>
-      )}
+      <Accordion
+        selectionMode="single"
+        className="text-gray-800 dark:text-gray-100"
+      >
+        {companies.map((company, index) => (
+          <AccordionItem
+            key={index}
+            aria-label={company.name}
+            indicator={({ isOpen }) =>
+              isOpen ? <HiOutlineArrowDown /> : <HiOutlineArrowUp />
+            }
+            startContent={
+              <img
+                src={company.logo}
+                alt={`${company.name} logo`}
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            }
+            subtitle={`${company.reviews} reviews`}
+            title={company.name}
+          >
+            <Card>
+              <Card>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <strong>Location:</strong> {company.location}
+                </p>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <strong>Global Company Size:</strong> {company.size}
+                </p>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <strong>Industry:</strong> {company.industry}
+                </p>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <strong>Reviews:</strong> {company.reviews}
+                </p>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <strong>Salaries:</strong> {company.salaries}
+                </p>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <strong>Jobs:</strong> {company.jobs}
+                </p>
+                <p className="mt-2 text-gray-700 dark:text-gray-300">
+                  {company.description}
+                </p>
+              </Card>
+            </Card>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 };
