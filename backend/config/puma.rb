c77a3ee6419ee0ea18e3c28a1ebc04e7dc646a9b
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-08-12 23:44:32 +0000

    fix: worker booting

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -3,7 +3,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
+# Specifies the worker count for production
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
@@ -11,13 +11,13 @@ if ENV["RAILS_ENV"] == "production"
 end
 
 # Specifies the port that Puma will listen on to receive requests
-port ENV.fetch("PORT") {8080}
-# bind "tcp://0.0.0.0:#{ENV.fetch('$PORT', 8080)}"
+# port ENV.fetch("PORT") { 8080 }
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
-end
+# Bind to all available network interfaces
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 8080)}"
 
+# Allows Puma to be restarted by `bin/rails restart` command
+plugin :tmp_restart
 
 # Specifies the environment that Puma will run in
 environment ENV.fetch("RAILS_ENV") { "production" }
@@ -25,5 +25,6 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 # Specifies the `pidfile` that Puma will use
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Allow Puma to be restarted by `bin/rails restart` command
-plugin :tmp_restart
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+end
