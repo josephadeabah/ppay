2d77a6216526b0ef729155ddd13738c597a880e8
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-08-12 23:57:19 +0000

    feat: deployed backend

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -1,14 +1,14 @@
 # Set the number of threads to use
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# threads min_threads_count, max_threads_count
 
 # Specifies the worker count for production
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
+# if ENV["RAILS_ENV"] == "production"
+#   require "concurrent-ruby"
+#   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+#   workers worker_count if worker_count > 1
+# end
 
 # Specifies the port that Puma will listen on to receive requests
 # port ENV.fetch("PORT") { 8080 }
