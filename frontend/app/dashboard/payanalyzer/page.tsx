e289838d49b16e6824bf0c1bfd83e29e0d88185a
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-08-24 06:50:04 +0000

    feat: add colors to analyzer ops buttons

--- frontend/app/dashboard/payanalyzer/page.tsx
+++ frontend/app/dashboard/payanalyzer/page.tsx
@@ -131,7 +131,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Scan the data for potential issues or trends. Visually mark or color-code rows where discrepancies or significant differences are detected."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <AiOutlineScan className="h-4 w-4" />
+                              <AiOutlineScan className="h-4 w-4 text-blue-500" />
                               <span className="hidden sm:inline">Scan</span>
                             </button>
 
@@ -140,7 +140,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Search the data for specific terms or criteria."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <AiOutlineSearch className="h-4 w-4" />
+                              <AiOutlineSearch className="h-4 w-4 text-teal-500" />
                               <span className="hidden sm:inline">Search</span>
                             </button>
 
@@ -149,7 +149,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Narrow down the data by specific criteria (e.g., gender, ethnicity, department, or salary range)."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <BiFilterAlt className="h-4 w-4" />
+                              <BiFilterAlt className="h-4 w-4 text-orange-500" />
                               <span className="hidden sm:inline">Filter</span>
                             </button>
 
@@ -158,7 +158,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Organize the data in ascending or descending order by specific columns (e.g., base salary, years of experience)."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <MdSort className="h-4 w-4" />
+                              <MdSort className="h-4 w-4 text-green-500" />
                               <span className="hidden sm:inline">Sort</span>
                             </button>
 
@@ -167,7 +167,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Perform aggregate calculations like average, median, or sum for columns such as salaries or bonuses."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <HiOutlineChartPie className="h-4 w-4" />
+                              <HiOutlineChartPie className="h-4 w-4 text-yellow-500" />
                               <span className="hidden sm:inline">
                                 Aggregate
                               </span>
@@ -178,7 +178,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Compare multiple rows of data to analyze differences in salary, bonus, or other compensation components."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <MdOutlineCompareArrows className="h-4 w-4" />
+                              <MdOutlineCompareArrows className="h-4 w-4 text-red-500" />
                               <span className="hidden sm:inline">Compare</span>
                             </button>
 
@@ -187,7 +187,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Add notes or comments to specific rows or cells for context or additional information."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <HiOutlineAnnotation className="h-4 w-4" />
+                              <HiOutlineAnnotation className="h-4 w-4 text-violet-500" />
                               <span className="hidden sm:inline">Annotate</span>
                             </button>
                             {/* Merge Button */}
@@ -195,7 +195,7 @@ export default function PayAnalyzerPage() {
                               data-tip="Combine data from multiple sources or datasets, such as merging salary data with performance metrics."
                               className="flex w-full items-center gap-1 rounded-sm bg-slate-100 p-2 shadow-sm sm:w-auto"
                             >
-                              <FiGitMerge className="h-4 w-4" />
+                              <FiGitMerge className="h-4 w-4 text-indigo-500" />
                               <span className="hidden sm:inline">Merge</span>
                             </button>
                           </div>
