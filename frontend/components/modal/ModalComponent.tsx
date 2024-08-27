5e23ccd4c8ff9f41ffd7cbd0838b4f001f95e4d4
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-08-27 07:50:02 +0000

    fix: worldmap modal

--- frontend/components/modal/ModalComponent.tsx
+++ frontend/components/modal/ModalComponent.tsx
@@ -23,7 +23,7 @@ const ModalComponent: React.FC<ModalProps> = ({
         isOpen ? "flex" : "hidden"
       } fixed inset-0 z-50 overflow-y-auto bg-gray-900 bg-opacity-50 [&::-moz-scrollbar-thumb]:rounded-full [&::-moz-scrollbar-thumb]:bg-gray-200 [&::-moz-scrollbar-track]:m-1 [&::-moz-scrollbar]:w-2 [&::-ms-scrollbar-thumb]:rounded-full [&::-ms-scrollbar-thumb]:bg-gray-200 [&::-ms-scrollbar-track]:m-1 [&::-ms-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 [&::-webkit-scrollbar-track]:m-1 [&::-webkit-scrollbar]:w-2`}
     >
-      <div className="relative mx-auto my-6 w-full max-w-2xl p-4">
+      <div className="relative mx-auto my-6 w-full max-w-4xl p-4">
         <div className="relative flex h-full max-h-[calc(100vh-5rem)] flex-col rounded-lg bg-white shadow dark:bg-gray-700">
           <div className="flex items-center justify-between rounded-t border-b border-gray-200 p-4 dark:border-gray-600">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
